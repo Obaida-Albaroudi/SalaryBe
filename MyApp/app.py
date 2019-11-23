e1a183df2d26c0fcd6821743ee20455c62f5e497
Obaida-Albaroudi <o.albaroudi96@gmail.com>
2019-11-23 12:44:22 -0800

    Finished

--- MyApp/app.py
+++ MyApp/app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify;
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
-import sqlite3
 from flask_cors import CORS
 import psycopg2
 
